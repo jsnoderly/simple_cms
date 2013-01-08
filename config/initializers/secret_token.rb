5e2ffb7c64cac763832e83d741ab86ca643de624
Johnathan Snoderly <jsnoderly@breproperties.com>
2013-01-07 17:03:21 -0800

    Created rails application for simple CMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'f863dfd57f751cc0ba26375de11194c23a051bb91c21dbfae1a603896fa9a30737b81f22fc933183e7befab1a4f80ef661152e8ddb61b70755a5524f6eafe039'
